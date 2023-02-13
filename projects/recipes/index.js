2061a231632c7b873281a8416ded0d2bc8290741
Kristy Bui <bui.kristy@gmail.com>
2023-02-12 17:51:37 -0800

    Add styling

--- projects/recipes/index.js
+++ projects/recipes/index.js
@@ -2,6 +2,18 @@ const http = require('http');
 const express = require('express');
 // const db = require('./js/database.js');
 
+// this is for refreshing the browser on changes
+var livereload = require("livereload"); 
+var connectLiveReload = require("connect-livereload");
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+///////////
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -9,6 +21,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(express.static("css"));
+app.use(connectLiveReload()); // part of development for refreshing on change
+
 
 
 app.get("/", function(req, res) {
